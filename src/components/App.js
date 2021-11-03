ed947e3a7f20e009aff44f3423ff543860b3f655
Mark <mark.nelubin@gmail.com>
2021-11-03 03:47:17 +0300

    fixed UI and UX bugs, added cuid update logic

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import ChatRoom from './ChatRoom';
 import Message from '../uitils/Message';
 
 // Подгружаем историю из локального хранилища
-const getLocalStorage = () => {
+const getLocalMessages = () => {
   let messages = localStorage.getItem('messages');
   if (messages) {
     return JSON.parse(localStorage.getItem('messages'))
@@ -13,12 +13,24 @@ const getLocalStorage = () => {
   }
 }
 
+const getLocalCuid = () => {
+  let cuid = localStorage.getItem('cuid');
+  if (cuid) {
+    return JSON.parse(localStorage.getItem('cuid'))
+  } else {
+    return ''
+  }
+}
+
 function App() {
-  const [messages, setMessages] = useState(getLocalStorage());
-  const [cuid, setCuid] = useState('');
+  const [messages, setMessages] = useState(getLocalMessages());
+  const [cuid, setCuid] = useState(getLocalCuid());
   const [lastBotMsg, setLastBotMsg] = useState('');
   const [lastUserMsg, setLastUserMsg] = useState('');
+  // это значение нужно нам для ответа каждый раз, когда пользователь вводит повторное сообщение. Используется в качестве зависимости в useEffect.
+  const countUserMessages = messages.filter(msg => msg.type === 'sent').length;
 
+  // данные для формирования запроса
   const uuid = '772c9859-4dd3-4a0d-b87d-d76b9f43cfa4';
   const euid = '00b2fcbe-f27f-437b-a0d5-91072d840ed3';
   const url = 'https://biz.nanosemantics.ru/api/bat/nkd/json/Chat.';
@@ -69,10 +81,15 @@ function App() {
       // обновляем идентификатор чата когда происходит инициализация
       if (inputType === urlType.init) {
         setCuid(data.result.cuid)
-      } else {
-        // когда событие READY (приветственное сообщение) или метод REQUEST (ответ на запрос пользователя) - обновляем последнее сообщение
+      }
+      // когда событие READY (приветственное сообщение) или метод REQUEST (ответ на запрос пользователя) - обновляем последнее сообщение
+      else {
+        // проверяем, если сервер вернул новый cuid - обновляем его везде. Если хотим, чтобы при этом очищался чат и история - нужно вызвать resetChat (не уверен, что так нужно, оставляю только обновление cuid)
+        urlType.request && data.result.cuid && data.result.cuid !== cuid && setCuid(data.result.cuid);
         const textValue = data.result.text.value;
         textValue && setLastBotMsg(textValue);
+        // отправляем сообщение от бота каждый раз, когда оно является приветственным или когда оно хранится в стейте. Это нужно, чтобы избежать дупликатов при обновлении страницы.
+        (lastBotMsg || textValue === 'Здравствуйте.') && addBotMsg(textValue);
       }
     } catch (error) {
       throw error;
@@ -83,7 +100,7 @@ function App() {
     const newMsg = new Message(msg, 'received');
     setMessages([...messages, newMsg]);
   }
-
+  // перезапуск разговора в новой сессии с удалением истории текущего диалога
   const resetChat = () => {
     localStorage.clear();
     setMessages([]);
@@ -92,32 +109,36 @@ function App() {
     setLastUserMsg('');
     fetchData(urlType.init)
   };
-
+  // получаем cuid при первом рендере (в случае, если у нас нет cuid в локальном хранилище)
   useEffect(() => {
-    // fetchCuid(initURL, initParams);
-    fetchData(urlType.init)
+    !cuid && fetchData(urlType.init)
+    // eslint-disable-next-line
   }, []);
-
+  // получаем приветственное сообщение
   useEffect(() => {
     cuid && fetchData(urlType.event);
+    // eslint-disable-next-line
   }, [cuid]);
 
+  // сохраняем последнее отправленное сообщение пользователя для формирования запроса на реакцию бота
+  // каждый раз, когда появляется пользовательское сообщение. Мы используем countUserMessages вместо lastUserMsg, чтобы обрабатывать повторные сообщения пользователя (3раза ввел lte = 3 раза получил ответы)
   useEffect(() => {
-    lastBotMsg && addBotMsg(lastBotMsg);
-  }, [lastBotMsg]);
-
-  useEffect(() => {
-    lastUserMsg && fetchData(urlType.request);
-  }, [lastUserMsg]);
+    fetchData(urlType.request);
+    // eslint-disable-next-line
+  }, [countUserMessages]);
 
   useEffect(() => {
     localStorage.setItem('messages', JSON.stringify(messages))
   }, [messages])
 
+  useEffect(() => {
+    localStorage.setItem('cuid', JSON.stringify(cuid))
+  }, [cuid])
+
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1>🤖💬</h1>
         <button onClick={resetChat}>Очистить</button>
       </header>
       <section className='chatRoom'>
